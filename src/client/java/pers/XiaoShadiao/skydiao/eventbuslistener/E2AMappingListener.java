c5522ccb669020eb755b67cb45ccad795dd1b45a
小沙雕 <3381949033@qq.com>
2026-02-27 17:53:15 +0800

    version 0.0.3 | Release

--- src/client/java/pers/XiaoShadiao/skydiao/eventbuslistener/E2AMappingListener.java
+++ src/client/java/pers/XiaoShadiao/skydiao/eventbuslistener/E2AMappingListener.java
@@ -1,19 +1,26 @@
 package pers.XiaoShadiao.skydiao.eventbuslistener;
 
-import it.unimi.dsi.fastutil.objects.Object2ObjectAVLTreeMap;
-import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientWorldEvents;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.decoration.ArmorStand;
+import pers.XiaoShadiao.skydiao.utils.ToolList;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class E2AMappingListener extends AbstractListener {
 
-    public final E2AMapping e2aMapping = new E2AMapping();
+    public static final Pattern timeMatcher = Pattern.compile("[\\d]+:[\\d]+");
+    public static final Pattern HIT_PATTERN = Pattern.compile("\\d+(?= Hit)");
+
+    private int tickCount = 0;
+    private final E2AMapping e2aMapping = new E2AMapping();
 
     @Override
     public String getListenerName() {
@@ -23,12 +30,94 @@ public class E2AMappingListener extends AbstractListener {
     @Override
     protected void registerListeners() {
         ClientTickEvents.START_CLIENT_TICK.register(this::onClientTick);
+        ClientWorldEvents.AFTER_CLIENT_WORLD_CHANGE.register(this::onWorldUnload);
+    }
+
+    private void onWorldUnload(Minecraft minecraft, ClientLevel clientLevel) {
+        e2aMapping.clear();
     }
 
     private void onClientTick(Minecraft mc) {
-        if(mc.level != null) {
-            e2aMapping.selfCleaning();
+        if(tickCount++ % 10 == 0 && mc.level != null) {
+            e2aMapping.selfCleaningAndUpdate();
+
+            // 获取所有盔甲架实体
+            java.util.List<ArmorStand> armorStands = new java.util.ArrayList<>();
+            java.util.List<ArmorStand> armorStandsForBoss = new java.util.ArrayList<>();
             for (Entity entity : mc.level.entitiesForRendering()) {
+                if (entity instanceof ArmorStand armorStand) {
+                    // 跳过已被绑定的盔甲架
+                    if (getLivingEntity(armorStand) != null) {
+                        continue;
+                    }
+                    String name = armorStand.getName().getString();
+                    Matcher matcher = timeMatcher.matcher(name);
+                    if (matcher.matches()) {
+                        armorStandsForBoss.add(armorStand);
+                        continue;
+                    }
+
+                    if (!name.contains("❤") && !name.contains(":")) {
+                        continue;
+                    }
+                    armorStands.add(armorStand);
+                }
+            }
+
+            // 为每个非盔甲架实体找到最近的盔甲架
+            for (Entity entity : mc.level.entitiesForRendering()) {
+
+                MobInfo info = null;
+                // 跳过盔甲架本身
+                infoLabel: {
+                    if (entity instanceof ArmorStand) {
+                        continue;
+                    }
+
+                    // 跳过已经是映射中的实体
+                    if (entity instanceof LivingEntity livingEntity) {
+                        info = getMobInfo(livingEntity);
+                        if (info != null) {
+                            break infoLabel;
+                        }
+                    }
+
+                    // 查找距离0.5内最近的未绑定盔甲架
+                    ArmorStand closestArmorStand = null;
+                    double minDistance = 1;
+
+                    double currentDistance = Integer.MAX_VALUE;
+                    for (ArmorStand armorStand : armorStands) {
+                        double distance = getXZDistance(entity, armorStand);
+                        if (distance <= minDistance && armorStand.getY() + 1 >= entity.getY() &&
+                                (closestArmorStand == null || distance < currentDistance)) {
+                            currentDistance = distance;
+                            closestArmorStand = armorStand;
+                        }
+                    }
+
+                    // 如果找到了符合条件的盔甲架，建立绑定
+                    if (closestArmorStand != null && entity instanceof LivingEntity livingEntity) {
+                        info = e2aMapping.addMapping(livingEntity, closestArmorStand);
+                    }
+                }
+
+                if(info != null) {
+                    double currentDistance = Integer.MAX_VALUE;
+                    double minDistance = 1;
+                    ArmorStand closestArmorStandForBoss = null;
+                    for (ArmorStand armorStand : armorStandsForBoss) {
+                        double distance = getXZDistance(entity, armorStand);
+                        if (distance <= minDistance && armorStand.getY() + 1 >= entity.getY() &&
+                                (closestArmorStandForBoss == null || distance < currentDistance)) {
+                            currentDistance = distance;
+                            closestArmorStandForBoss = armorStand;
+                        }
+                    }
+                    if (closestArmorStandForBoss != null) {
+                        info.armorStandForBoss = closestArmorStandForBoss;
+                    }
+                }
 
             }
         }
@@ -42,14 +131,29 @@ public class E2AMappingListener extends AbstractListener {
         return e2aMapping.getLivingEntity(a);
     }
 
-    public class E2AMapping {
+    public MobInfo getMobInfo(LivingEntity e) {
+        return e2aMapping.getMobInfo(e);
+    }
+
+    public static class E2AMapping {
         private final Map<LivingEntity, ArmorStand> e2a = new HashMap<>();
         private final Map<ArmorStand, LivingEntity> a2e = new HashMap<>();
+        private final Map<LivingEntity, MobInfo> e2Info = new HashMap<>();
 
-        public void addMapping(LivingEntity e, ArmorStand a) {
-            if(e == null) return;
+        public MobInfo addMapping(LivingEntity e, ArmorStand a) {
+            if(e == null || a == null) return null;
             e2a.put(e, a);
             a2e.put(a, e);
+            MobInfo mobInfo = new MobInfo();
+            mobInfo.armorStand = a;
+            mobInfo.maxHealth = Math.max(e.getHealth(), e.getMaxHealth());
+            mobInfo.health = e.getHealth();
+            mobInfo.hasHittingAttr = false;
+            mobInfo.hit = -1;
+            mobInfo.maxHit = -1;
+            mobInfo.isBoss = false;
+            e2Info.put(e, mobInfo);
+            return mobInfo;
         }
 
         public void clear() {
@@ -57,18 +161,62 @@ public class E2AMappingListener extends AbstractListener {
             a2e.clear();
         }
 
-        public void selfCleaning() {
-            e2a.entrySet().removeIf(entry -> mc.level != null && (mc.level.getEntity(entry.getKey().getId()) == null || mc.level.getEntity(entry.getValue().getId()) == null));
-            a2e.entrySet().removeIf(entry -> mc.level != null && (mc.level.getEntity(entry.getKey().getId()) == null || mc.level.getEntity(entry.getValue().getId()) == null));
+        public void selfCleaningAndUpdate() {
+            e2a.entrySet().removeIf(entry -> mc.level == null || (mc.level.getEntity(entry.getKey().getId()) == null || mc.level.getEntity(entry.getValue().getId()) == null));
+            a2e.entrySet().removeIf(entry -> mc.level == null || (mc.level.getEntity(entry.getKey().getId()) == null || mc.level.getEntity(entry.getValue().getId()) == null));
+
+            e2Info.entrySet().removeIf(entry -> mc.level == null || !e2a.containsKey(entry.getKey()));
+            e2Info.forEach((key, mobInfo) -> {
+                mobInfo.health = key.getHealth();
+
+                Matcher m = HIT_PATTERN.matcher(ToolList.getInstance().deleteColorCode(mobInfo.armorStand.getName().getString()));
+                if(m.find()) {
+                    mobInfo.hit = ToolList.getInstance().isDevEnvironment() ? (int) mobInfo.health : Integer.parseInt(m.group());
+                    if(!mobInfo.hasHittingAttr) {
+                        mobInfo.hasHittingAttr = true;
+                        mobInfo.maxHit = mobInfo.hit;
+                    }
+                    mobInfo.maxHit = Math.max(mobInfo.hit, mobInfo.maxHit);
+                } else {
+                    mobInfo.hasHittingAttr = false;
+                    mobInfo.hit = mobInfo.maxHit = -1;
+                }
+            });
         }
 
         public ArmorStand getArmorStand(LivingEntity e) {
+            if(e == null) return null;
             return e2a.get(e);
         }
 
+        public MobInfo getMobInfo(LivingEntity e) {
+            if(e == null) return null;
+            return e2Info.get(e);
+        }
+
         public LivingEntity getLivingEntity(ArmorStand a) {
+            if(a == null) return null;
             return a2e.get(a);
         }
+    }
+
+    private double getXZDistance(Entity e1, Entity e2) {
+        return Math.sqrt((e1.getX() - e2.getX()) * (e1.getX() - e2.getX()) + (e1.getZ() - e2.getZ()) * (e1.getZ() - e2.getZ()));
+    }
+
+    private double getYDistance(Entity e1, Entity e2) {
+        return Math.abs(e1.getY() - e2.getY());
+    }
+
+    public static class MobInfo {
+        public ArmorStand armorStand;
+        public double maxHealth;
+        public double health;
+        public int hit;
+        public int maxHit;
+        public boolean isBoss;
+        public ArmorStand armorStandForBoss;
+        public boolean hasHittingAttr;
     }
 
 }
